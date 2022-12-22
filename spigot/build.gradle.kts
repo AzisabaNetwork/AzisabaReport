9adc7fd5135f7f9a905d9c02163da1e1be367352
acrylic-style <me@acrylicstyle.xyz>
2022-12-22 21:23:07 +0900

    fix: rollback useless change

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -1,5 +1,4 @@
 repositories {
-    mavenLocal()
     maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
     maven { url = uri("https://ci.emc.gs/nexus/content/repositories/aikar-release/") }
@@ -9,8 +8,7 @@ repositories {
 dependencies {
     api(project(":common"))
     compileOnly("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
-    compileOnly("net.azisaba:RyuZUPluginChat:4.2.0")
-    //compileOnly("com.github.AzisabaNetwork:RyuZUPluginChat:v4.2.0")
+    compileOnly("com.github.AzisabaNetwork:RyuZUPluginChat:v4.1.0")
     compileOnly("com.github.AzisabaNetwork:LunaChatPlus:v3.2.0") {
         exclude("org.bstats", "bstats-bukkit")
         exclude("org.bstats", "bstats-bungeecord")
